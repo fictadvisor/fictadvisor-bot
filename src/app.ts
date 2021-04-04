6f7e2b819f3a414da2f0388cc9c056fed3a66baa
Alexander Nazarov <alxkrekeris@gmail.com>
2021-04-04 19:11:23 +0300

    added debug command

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import { Telegraf } from "telegraf";
 import login from './actions/login';
 import start from "./actions/start";
 import cancelLogin from "./actions/cancelLogin";
+import debug from "./actions/debug";
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.hears(/^(\/start login|\/login)$/g, login());
 bot.start(start());
+bot.command('/debug', debug());
 bot.on('callback_query', cancelLogin());
 
 export default bot;
