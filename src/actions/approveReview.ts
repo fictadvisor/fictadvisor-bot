78ebee92de76bc0a7e257b3e483e47e921363248
Alexander Nazarov <alxkrekeris@gmail.com>
2021-04-09 22:22:26 +0300

    added better response to error

--- src/actions/approveReview.ts
+++ src/actions/approveReview.ts
@@ -1,13 +1,14 @@
 import api from "../api";
 import { Context } from "telegraf";
+import { AxiosError } from 'axios';
 
 export default () => {
     return async (ctx: Context) => {
-        try {
-            const id = (ctx.callbackQuery as any).data.split(':')[1];
-            await api.reviews.update(id, { state: 'approved' });
+        const id = (ctx.callbackQuery as any).data.split(':')[1];
+        const { message, inline_message_id, from } = ctx.callbackQuery;
 
-            const { message, inline_message_id, from } = ctx.callbackQuery;
+        try {
+            await api.reviews.update(id, { state: 'approved' });
 
             await ctx.telegram.editMessageText(
                 message.chat.id, 
@@ -26,8 +27,24 @@ export default () => {
                 }
             );
         } catch (e) {
+            const axiosError = e as AxiosError;
+
+            if (axiosError.isAxiosError) {
+                if (axiosError.response?.status === 404) {
+                    await ctx.telegram.editMessageText(
+                        message.chat.id, 
+                        message.message_id,
+                        inline_message_id,
+                        `<b>Відгук ${id} вже було видалено.</b>`
+                    );
+
+                    return;
+                }
+            }
+
             console.error(e);
-            await ctx.reply(`Щось пішло не так: ${e}`);
+            
+            await ctx.reply(e.toString(), { reply_to_message_id: message.message_id });
         }
     };
 };
--- src/actions/denyReview.ts
+++ src/actions/denyReview.ts
@@ -1,14 +1,14 @@
 import api from "../api";
 import { Context } from "telegraf";
+import { AxiosError } from 'axios';
 
 export default () => {
     return async (ctx: Context) => {
+        const id = (ctx.callbackQuery as any).data.split(':')[1];
+        const { message, inline_message_id, from } = ctx.callbackQuery;
+
         try {
-            const id = (ctx.callbackQuery as any).data.split(':')[1];
-
             await api.reviews.delete(id);
-
-            const { message, inline_message_id, from } = ctx.callbackQuery;
 
             await ctx.telegram.editMessageText(
                 message.chat.id, 
@@ -22,8 +22,24 @@ export default () => {
                 }
             );
         } catch (e) {
+            const axiosError = e as AxiosError;
+
+            if (axiosError.isAxiosError) {
+                if (axiosError.response?.status === 404) {
+                    await ctx.telegram.editMessageText(
+                        message.chat.id, 
+                        message.message_id,
+                        inline_message_id,
+                        `<b>Відгук ${id} вже було видалено.</b>`
+                    );
+
+                    return;
+                }
+            }
+
             console.error(e);
-            await ctx.reply(`Щось пішло не так: ${e}`);
+
+            await ctx.reply(e.toString(), { reply_to_message_id: message.message_id });
         }
     };
 };
