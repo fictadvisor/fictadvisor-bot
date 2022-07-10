032f9e6746129a87af0744b123a7f3ec793d0172
just-hoshinon <mwadman2018@gmail.com>
2022-07-11 01:44:02 +0300

    updating course information

--- src/actions/approve.course.ts
+++ src/actions/approve.course.ts
@@ -8,9 +8,10 @@ export class ApproveCourse extends ApproveAction {
     return `deny_course:${this.id}`;
   }
 
-  createMessage (data: {name: string}): string {
+  createMessage (data: {name: string, teacher: {first_name: string, middle_name: string, last_name: string}}): string {
     return `<b>🟢 Додавання курсу ${this.id} схвалено.</b>\n\n` +
-      `<b>Назва курсу:</b> ${data.name}\n\n` +
+      `<b>Назва курсу:</b> ${data.name}\n` +
+      `<b>ПІБ Викладача:</b> ${data.teacher.last_name} ${data.teacher.first_name} ${data.teacher.middle_name}\n\n` +
       `<b>Ким:</b> <a href="tg://user?id=${this.from.id}">${this.from.username ? `@${this.from.username}` : this.from.first_name}</a>\n` +
       `<b>Коли:</b> ${new Date().toISOString()}`;
   }
--- src/actions/deny.course.ts
+++ src/actions/deny.course.ts
@@ -4,9 +4,10 @@ import api from '../api';
 export class DenyCourse extends Action {
   item_name = 'Курс';
 
-  createMessage (data: {name: string}): string {
+  createMessage (data: {name: string, teacher: {first_name: string, middle_name: string, last_name: string}}): string {
     return `<b>🔴 Додавання курсу ${this.id} відхилено.</b>\n\n` +
-        `<b>Назва курсу:</b> ${data.name}\n\n` +
+        `<b>Назва курсу:</b> ${data.name}\n` +
+        `<b>ПІБ Викладача:</b> ${data.teacher.last_name} ${data.teacher.first_name} ${data.teacher.middle_name}\n\n` +
         `<b>Ким:</b> <a href="tg://user?id=${this.from.id}">${this.from.username ? `@${this.from.username}` : this.from.first_name}</a>\n` +
         `<b>Коли:</b> ${new Date().toISOString()}`;
   }
