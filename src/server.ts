8c765576a83a5f58726aa96c9b6263f04120a7cc
Kulunchick <nikolikrasovski@gmail.com>
2022-12-25 14:24:16 +0200

    UPD: Add Singleton Telegraf and connect express

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import routes from './api/v1/index'
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
+
+server.use('/api/v1', routes)
 
 export default server;
