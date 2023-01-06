681bd524c547534f9f386f421bf9bce144fc9652
thebladehit <boqdanyarmolka@gmail.com>
2023-01-07 01:04:07 +0200

    added teacher service...

--- src/api/v1/teachers/teachers.service.ts
+++ src/api/v1/teachers/teachers.service.ts
@@ -1,5 +1,30 @@
+import TelegramService  from '../../../telegram/telegram.sevice';
+
 export class TeachersService {
-    static broadcastPending(data) {
-        return data;
+    static async broadcastPending(data) {
+        const bot = TelegramService.getInstance();
+        const chatId = process.env.CHAT_ID;
+        await bot.telegram.sendMessage(chatId,`<b>Заявка на додавання викладача</b>\n\n` +
+            `<b><a href="${process.env.FRONT_BASE_URL}/teachers/${data.teacher.link}">${data.teacher.fullname}</a></b> (${data.teacher.id})\n` +
+            `<b>Автор</b>: ${data.user.first_name}`,
+            {
+                parse_mode: 'HTML',
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            {
+                                text: 'Схвалити',
+                                callback_data: `approve_teacher:${data.teacher.id}:${data.user.telegramId}`,
+                            },
+                        ],
+                        [
+                            {
+                                text: 'Відмовити',
+                                callback_data: `deny_teacher:${data.teacher.id}:${data.user.telegramId}`,
+                            },
+                        ],
+                    ],
+                },
+            });
     }
 }
