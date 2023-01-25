3e1fa36a610fec93587236d99a466d7d0c772a34
Kulunchick <nikolikrasovski@gmail.com>
2023-01-25 17:45:57 +0200

    UPD: removed duplicate code

--- src/api/v1/verification/verification.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { VerificationController } from './verification.controller';
-
-const router = Router();
-
-router.route('/verification/captain').post(VerificationController.verifyCaptain)
-router.route('/verification/student').post(VerificationController.verifyStudent)
-
-export default router;
