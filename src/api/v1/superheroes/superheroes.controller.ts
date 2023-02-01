85511e59290589940b4ab017ea94eb3673db8436
Kulunchick <nikolikrasovski@gmail.com>
2023-02-01 18:18:05 +0200

    UPD: Added awaits for functions

--- src/api/v1/superheroes/superheroes.controller.ts
+++ src/api/v1/superheroes/superheroes.controller.ts
@@ -3,7 +3,7 @@ import { SuperheroesService } from './superheroes.service';
 export class SuperheroesController {
   static async broadcastPending(req, res) {
     try {
-      const data = SuperheroesService.broadcastPending(req.body);
+      const data = await SuperheroesService.broadcastPending(req.body);
       return res.status(200).send({message: data});
     } catch(err) {
       return res.status(400).send({message: 'An exception occured while sending message'});
