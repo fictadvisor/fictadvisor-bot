8ee7facbea458fb021aa3a6aacdc3e09804a4ea8
Kulunchick <nikolikrasovski@gmail.com>
2023-02-22 19:50:06 +0200

    UPD: add authorization on start

--- src/actions/start.ts
+++ src/actions/start.ts
@@ -1,9 +1,23 @@
 import { Context } from 'telegraf';
+import { v4 as uuidv4 } from 'uuid';
+import {client} from "../api";
+import NodeCache from "node-cache";
+
+const cache = new NodeCache({stdTTL: 60*60});
 
 export default () => async (ctx: Context) => {
-  const url = `${process.env.FRONT_BASE_URL}/oauth`;
+  let id = cache.get(ctx.from.id);
+  if (id == undefined) {
+    id = uuidv4();
+    cache.set(ctx.from.id, id);
+  }
+  const url = `${process.env.FRONT_BASE_URL}/oauth?token=${id}`;
+  await client.post(`/auth/registerTelegram`, {
+    token: id,
+    telegramId: ctx.from.id,
+  });
 
-  await ctx.reply(`<b>Вітаємо вас у боті <a href="https://fictadvisor.com">fictadvisor.com</a></b>\n` +
+  await ctx.reply(`<b>Вітаємо вас у боті <a href="${process.env.FRONT_BASE_URL}">fictadvisor.com</a></b>\n` +
                   `Зворотній зв'язок: @fict_robot`, { parse_mode: 'HTML' });
   await ctx.reply(`<b>Для реєстрації перейдіть за почиланням</b>\n\n` +
                   `Якщо кнопка не працює, тицьни <a href="${url}">сюди</a>.`,
