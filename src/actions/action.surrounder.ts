241c1f9a413b07fec54e43de6805911a5aab6ca5
just-hoshinon <mwadman2018@gmail.com>
2023-02-26 04:59:37 +0200

    added checking

--- src/actions/action.surrounder.ts
+++ src/actions/action.surrounder.ts
@@ -24,6 +24,7 @@ export default abstract class Action {
   }
 
   get id(): string {
+    console.log((this.context.callbackQuery as any).data);
     return (this.context.callbackQuery as any).data.split(':')[2];
   }
 
