1ac58b0e0130596e6855f1551ef037a48fee317e
Kulunchick <nikolikrasovski@gmail.com>
2023-03-24 19:20:28 +0200

    UPD: Removed debug print

--- app/services/response_api.py
+++ app/services/response_api.py
@@ -9,5 +9,4 @@ class ResponseAPI(BaseAPI):
 
     async def verify_response(self, response_id: UUID, data: Dict[str, Any]) -> Dict[str, Any]:
         async with self._session.post(f"{self.path}/{response_id}/responses", json=data) as response:
-            print(response.url)
             return await response.json()
