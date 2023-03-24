acdbc2f91fbe9648be4c9d91ee583d6a4dd28e54
Kulunchick <nikolikrasovski@gmail.com>
2023-03-24 19:45:33 +0200

    UPD: Added UserAPI

---
+++ app/services/user_api.py
@@ -0,0 +1,27 @@
+from enum import Enum
+from typing import Any, Dict
+from uuid import UUID
+
+from app.services.base_api import BaseAPI
+
+
+class State(str, Enum):
+    PENDING = "PENDING"
+    APPROVED = 'APPROVED'
+    DECLINED = 'DECLINED'
+
+
+class UserAPI(BaseAPI):
+    _path = "/users"
+
+    async def verify_student(self, student_id: UUID, state: State, is_captain: bool) -> Dict[str, Any]:
+        async with self._session.patch(f"{self.path}/{student_id}/verifyStudent", json={"state": state, "isCaptain": is_captain}) as response:
+            return await response.json()
+
+    async def verify_superhero(self, student_id: UUID, state: State, is_captain: bool) -> Dict[str, Any]:
+        async with self._session.patch(f"{self.path}/{student_id}/verifyStudent", json={"state": state, "isCaptain": is_captain}) as response:
+            return await response.json()
+
+    async def get_user(self, user_id: UUID) -> Dict[str, Any]:
+        async with self._session.get(f"{self.path}/{user_id}/telegram") as response:
+            return await response.json()
