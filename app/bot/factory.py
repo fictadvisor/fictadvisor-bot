c22f5b22963f48ecc794048408e87df4ffee8098
Kulunchick <nikolikrasovski@gmail.com>
2023-03-26 22:54:37 +0300

    UPD: Added captain verification callback

--- app/bot/factory.py
+++ app/bot/factory.py
@@ -5,12 +5,13 @@ from app.bot.handlers.debug import debug_router
 from app.bot.handlers.student import student_router
 from app.bot.handlers.superhero import superhero_router
 from app.bot.handlers.response import response_router
+from app.bot.handlers.captain import captain_router
 
 
 def create_dispatcher() -> Dispatcher:
     dispatcher = Dispatcher()
 
-    for router in [debug_router, student_router, superhero_router, response_router]:
+    for router in [debug_router, student_router, superhero_router, response_router, captain_router]:
         dispatcher.include_router(router)
 
     return dispatcher
