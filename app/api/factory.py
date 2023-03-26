58c0b188f01f2abc6ac2a2d6721615b518061521
Kulunchick <nikolikrasovski@gmail.com>
2023-03-26 18:49:22 +0300

    UPD: Added superheroes verification handler

--- app/api/factory.py
+++ app/api/factory.py
@@ -2,10 +2,12 @@ from functools import partial
 
 from aiogram import Dispatcher, Bot
 from fastapi import FastAPI, APIRouter, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.middlewares.authentication import verify_token
 from app.api.routes.response import response_router
 from app.api.routes.student import student_router
+from app.api.routes.superhero import superhero_router
 from app.api.routes.webhook import webhook_router
 from app.api.stubs import BotStub, DispatcherStub, SecretStub
 from app.custom_logging import CustomizeLogger
@@ -39,12 +41,20 @@ def create_app(bot: Bot, dispatcher: Dispatcher, webhook_secret: str) -> FastAPI
     logger = CustomizeLogger.make_logger(settings.LOG_LEVEL, settings.LOG_FORMAT)
     app.logger = logger
 
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.add_event_handler('startup', partial(on_startup, bot))
     app.add_event_handler('shutdown', partial(on_shutdown, bot))
     app.include_router(webhook_router)
 
     api = APIRouter(prefix="/api/v1", dependencies=[Depends(verify_token)])
-    for router in [response_router, student_router]:
+    for router in [response_router, student_router, superhero_router]:
         api.include_router(router)
 
     app.include_router(api)
