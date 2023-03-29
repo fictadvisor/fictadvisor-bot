8ddb542074da237d02d565d52ba66bc08bc8f074
Kulunchick <nikolikrasovski@gmail.com>
2023-03-29 13:27:59 +0300

    UPD: Added start command (and generate register link)

--- app/bot/factory.py
+++ app/bot/factory.py
@@ -2,6 +2,7 @@ from aiogram import Dispatcher, Bot
 from aiogram.enums import ParseMode
 
 from app.bot.handlers.debug import debug_router
+from app.bot.handlers.private import private_router
 from app.bot.handlers.student import student_router
 from app.bot.handlers.superhero import superhero_router
 from app.bot.handlers.response import response_router
@@ -11,7 +12,7 @@ from app.bot.handlers.captain import captain_router
 def create_dispatcher() -> Dispatcher:
     dispatcher = Dispatcher()
 
-    for router in [debug_router, student_router, superhero_router, response_router, captain_router]:
+    for router in [debug_router, student_router, superhero_router, response_router, captain_router, private_router]:
         dispatcher.include_router(router)
 
     return dispatcher
---
+++ app/messages/private.py
@@ -0,0 +1,14 @@
+from jinja2 import Environment
+
+environment = Environment(enable_async=True)
+
+START = environment.from_string("""
+<b>Вітаємо вас у боті <a href="{{ front_url }}">fictadvisor.com</a></b>
+Зворотній зв'язок: @fict_robot
+""")
+
+REGISTER = environment.from_string("""
+<b>Для реєстрації перейдіть за посиланням</b>
+
+Якщо кнопка не працює, тицьни <a href="{{ register_url }}">сюди</a> 
+""")
