1f864b25706ebf855eb714d8d6836e1f81cad49e
Kulunchick <nikolikrasovski@gmail.com>
2023-07-10 01:09:53 +0300

    FIX: Switch Telegram to Bearer scheme

--- app/api/middlewares/authentication.py
+++ app/api/middlewares/authentication.py
@@ -3,10 +3,10 @@ from fastapi.security import APIKeyHeader
 
 from app.settings import settings
 
-auth_header = APIKeyHeader(name="Authorization", scheme_name="Telegram", description="Token of Telegram Bot", auto_error=False)
+auth_header = APIKeyHeader(name="Authorization", scheme_name="Bearer", description="Token of Telegram Bot", auto_error=False)
 
 
 async def verify_token(token: str = Security(auth_header)):
-    if token != f"Telegram {settings.TOKEN.get_secret_value()}":
+    if token != f"Bearer {settings.TOKEN.get_secret_value()}":
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     return token
