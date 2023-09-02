c9d936b92b2ccb73d3b9d8dbac821c0ba3fe0fc1
Kulunchick <nikolikrasovski@gmail.com>
2023-09-03 00:01:15 +0300

    hotfix

--- app/messages/student.py
+++ app/messages/student.py
@@ -8,7 +8,7 @@ BROADCAST_STUDENT = environment.from_string("""
 <b>Від:</b> {{ data.last_name }} {{ data.first_name }} {{ data.middle_name|d('', true) }}
 
 {% if user %}
-<b>Юзернейм:</b> <a href="tg://user?id=${user.id}">{{ user.username|d(user.first_name, true) }}</a>
+<b>Юзернейм:</b> <a href="tg://user?id={{ user.id }}">{{ user.username|d(user.first_name, true) }}</a>
 {% endif %}
 <b>Група:</b> {{ data.group_code }}
 """)
