d22775b4ebbbcfd5bcf7030eab4ff55ffb75c1a0
GoReNeY <57534011+GoReNeY@users.noreply.github.com>
2023-10-04 19:12:23 +0300

    enhancement #83: Added post_info flag to TelegramGroup
    * Added post_info flag to TelegramGroup

--- app/services/types/teleram_group.py
+++ app/services/types/teleram_group.py
@@ -11,12 +11,14 @@ class UpdateTelegramGroup(Base):
     telegram_id: Optional[int] = Field(None, alias="telegramId")
     thread_id: Optional[int] = Field(None, alias="threadId")
     source: Optional[TelegramSource] = Field(None)
+    post_info: Optional[bool] = Field(None, alias="postInfo")
 
 
 class CreateTelegramGroup(Base):
     telegram_id: int = Field(alias="telegramId")
     thread_id: Optional[int] = Field(None, alias="threadId")
     source: TelegramSource
+    post_info: bool = Field(False, alias="postInfo")
 
 
 class TelegramGroup(CreateTelegramGroup):
