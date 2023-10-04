0e2e1cf9607c9db9317f4872f2d03bbb2df756cf
Kulunchick <nikolikrasovski@gmail.com>
2023-10-04 11:06:03 +0300

    feature #61: Implement commands for schedule

--- app/bot/handlers/schedule/__init__.py
+++ app/bot/handlers/schedule/__init__.py
@@ -1,3 +1,12 @@
 from aiogram import Router
+from aiogram.filters import Command
+
+from app.bot.handlers.schedule.today import today
+from app.bot.handlers.schedule.tomorrow import tomorrow
+from app.bot.handlers.schedule.week import week
 
 router = Router()
+
+router.message.register(today, Command("today"))
+router.message.register(tomorrow, Command("tomorrow"))
+router.message.register(week, Command("week"))
