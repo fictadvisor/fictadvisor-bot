58b0f8fb8d81be3a7e065e6eb524a3a24685cf9f
Kulunchick <nikolikrasovski@gmail.com>
2023-10-08 03:23:15 +0300

    enhancement #95: Add return user to filter IsCaptainOrDeputy

--- app/bot/handlers/group/bind.py
+++ app/bot/handlers/group/bind.py
@@ -1,34 +1,37 @@
 import logging
+from typing import Optional
 
 from aiogram.types import Message
 
 from app.services.exceptions.response_exception import ResponseException
 from app.services.telegram_group_api import TelegramGroupAPI
-from app.services.types.teleram_group import UpdateTelegramGroup
+from app.services.types.student import Student
+from app.services.types.teleram_group import TelegramGroup, UpdateTelegramGroup
 
 
-async def bind(message: Message) -> None:
+async def bind(message: Message, user: Student) -> None:
     try:
         async with TelegramGroupAPI() as telegram_group_api:
-            telegram_groups = await telegram_group_api.get_by_telegram_id(message.chat.id)
-            already_bound = any(telegram_group.thread_id == message.reply_to_message.message_id for telegram_group in telegram_groups.telegram_groups)  # type: ignore[union-attr]
-            for telegram_group in telegram_groups.telegram_groups:
-                if not already_bound:
-                    await telegram_group_api.update(
-                        telegram_group.group.id,
-                        message.chat.id,
-                        UpdateTelegramGroup(
-                            thread_id=message.reply_to_message.message_id  # type: ignore[union-attr]
-                        )
+            telegram_groups = await telegram_group_api.get_telegram_groups(user.group.id)
+            telegram_group: Optional[TelegramGroup] = next(filter(lambda x: x.telegram_id == message.chat.id, telegram_groups.telegram_groups), None)  # type: ignore
+            if not telegram_group:
+                return
+            if telegram_group.thread_id != message.reply_to_message.message_id:  # type: ignore[union-attr]
+                await telegram_group_api.update(
+                    user.group.id,
+                    message.chat.id,
+                    UpdateTelegramGroup(
+                        thread_id=message.reply_to_message.message_id  # type: ignore[union-attr]
                     )
-                else:
-                    await telegram_group_api.update(
-                        telegram_group.group.id,
-                        message.chat.id,
-                        UpdateTelegramGroup(
-                            thread_id=None
-                        )
+                )
+            else:
+                await telegram_group_api.update(
+                    user.group.id,
+                    message.chat.id,
+                    UpdateTelegramGroup(
+                        thread_id=None
                     )
-            await message.answer("Гілку прикріплено" if not already_bound else "Гілку відкріплено")
+                )
+            await message.answer("Гілку прикріплено" if telegram_group.thread_id != message.reply_to_message.message_id else "Гілку відкріплено")  # type: ignore[union-attr]
     except ResponseException as e:
         logging.error(e)
