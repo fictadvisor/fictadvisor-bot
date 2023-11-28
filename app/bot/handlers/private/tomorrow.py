8584ec2cc28908fbb1286e4a06e40845598e829b
Kulunchick <nikolikrasovski@gmail.com>
2023-11-28 09:55:36 +0200

    fix tomorrow in sunday

--- app/bot/handlers/private/tomorrow.py
+++ app/bot/handlers/private/tomorrow.py
@@ -10,7 +10,12 @@ async def tomorrow(message: Message) -> None:
     async with UserAPI() as user_api:
         user = await user_api.get_user_by_telegram_id(message.from_user.id)  # type: ignore[union-attr]
     async with ScheduleAPI() as schedule_api:
-        general_events = await schedule_api.get_general_group_events_by_day(user.group.id, user_id=user.id, day=DateService.get_current_day() + 1)
+        general_events = await schedule_api.get_general_group_events_by_day(
+            user.group.id,
+            user_id=user.id,
+            day=DateService.get_current_weekday(),
+            week=DateService.get_week_by_day(DateService.get_current_day() + 1)
+        )
 
     if not general_events.events:
         await message.answer("Пар немає")
--- app/utils/date_service.py
+++ app/utils/date_service.py
@@ -7,13 +7,21 @@ import pytz
 class DateService:
     START_SEMESTER = date(2023, 9, 3).timetuple().tm_yday
 
+    @staticmethod
+    def get_now() -> datetime:
+        return datetime.now(pytz.timezone("Europe/Kiev"))
+
     @classmethod
     def get_current_day(cls) -> int:
-        return datetime.now(pytz.timezone("Europe/Kiev")).date().timetuple().tm_yday - cls.START_SEMESTER
+        return cls.get_now().date().timetuple().tm_yday - cls.START_SEMESTER
 
     @classmethod
     def get_current_weekday(cls) -> int:
-        return datetime.now(pytz.timezone("Europe/Kiev")).date().timetuple().tm_wday
+        return cls.get_now().date().timetuple().tm_wday
+
+    @staticmethod
+    def get_week_by_day(day: int) -> int:
+        return math.ceil(day / 7)
 
     @classmethod
     def get_week(cls) -> int:
