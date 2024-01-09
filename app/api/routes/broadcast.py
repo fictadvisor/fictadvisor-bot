cca5554e48f61a7b843125b2bfb7c02c91ce9c3c
Kulunchick <nikolikrasovski@gmail.com>
2024-01-09 19:12:58 +0200

    Set parse_mode default to None

--- app/api/routes/broadcast.py
+++ app/api/routes/broadcast.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Optional
 
 from aiogram import Bot
 from aiogram.enums.parse_mode import ParseMode
@@ -15,10 +16,10 @@ broadcast_router = APIRouter(prefix="/broadcast", tags=["Broadcast"])
 @broadcast_router.post('/sendMessage')
 async def send_message_handler(
         message: Message,
-        parse_mode: ParseMode = ParseMode.HTML,
+        parse_mode: Optional[ParseMode] = None,
         chat_id: int = Query(default=settings.CHAT_ID, alias="id"),
         bot: Bot = Depends(BotStub)
 ) -> JSONResponse:
     logging.error(message.text)
-    await bot.send_message(chat_id=chat_id, text=message.text, parse_mode=None)
+    await bot.send_message(chat_id=chat_id, text=message.text, parse_mode=parse_mode)
     return JSONResponse(status_code=200, content={})
