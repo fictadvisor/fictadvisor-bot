887db84039667d11f1de83979a95d8acd684acba
Belikov Maxim <maximax6767@gmail.com>
2025-03-04 14:28:20 +0200

    fix tomorrow command

--- app/bot/handlers/group/tomorrow.py
+++ app/bot/handlers/group/tomorrow.py
@@ -6,19 +6,21 @@ from app.services.types.telegram_groups import TelegramGroupsByTelegramId
 from app.utils.date_service import DateService
 
 
-async def tomorrow(message: Message, telegram_groups: TelegramGroupsByTelegramId) -> None:
+async def tomorrow(
+    message: Message, telegram_groups: TelegramGroupsByTelegramId
+) -> None:
     for telegram_group in telegram_groups.telegram_groups:
         async with ScheduleAPI() as schedule_api:
             general_events = await schedule_api.get_general_group_events_by_day(
-                telegram_group.group.id,
-                day=(DateService.get_current_weekday() + 2) % 7,
-                week=DateService.get_week_by_day(DateService.get_current_day() + 1)
+                telegram_group.group.id, day=DateService.get_current_day() + 1
             )
 
         if not general_events.events:
             await message.reply(f"У групи {telegram_group.group.code} пар немає")
         else:
             await message.reply(
-                await EVENT_LIST.render_async(group=telegram_group.group.code, events=general_events.events),
-                disable_web_page_preview=True
+                await EVENT_LIST.render_async(
+                    group=telegram_group.group.code, events=general_events.events
+                ),
+                disable_web_page_preview=True,
             )
